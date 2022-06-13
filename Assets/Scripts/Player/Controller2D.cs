9c72ab5e6cb64d85ba9856f67b7e2de888d825f7
David Allison Silva de Souza <100738882+DavidSouzaLD@users.noreply.github.com>
2022-06-13 16:42:46 -0300

    Game folder created!

--- Assets/Scripts/Player/Controller2D.cs
+++ Assets/Scripts/Player/Controller2D.cs
@@ -1,14 +1,53 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(InputClass))]
 public class Controller2D : MonoBehaviour
 {
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private PhysicsMaterial2D physicsMat;
+
+    [Header("Jump")]
+    [SerializeField] private float jumpForce;
+
+    Rigidbody2D Body2D;
+    BoxCollider2D BoxCol2D;
+    CircleCollider2D CircleCol2D;
+    InputClass Input;
+
     private void Start()
     {
+        Body2D = GetComponent<Rigidbody2D>();
+        BoxCol2D = GetComponent<BoxCollider2D>();
+        CircleCol2D = GetComponent<CircleCollider2D>();
+        Input = GetComponent<InputClass>();
 
+        CircleCol2D.sharedMaterial = physicsMat;
     }
 
     private void Update()
     {
+        Vector2 _moveAxis = Input.MoveAxis();
+        Vector2 _currentVelocity = new Vector2(moveSpeed * _moveAxis.x, 0);
 
+        if (_moveAxis.x != 0)
+        {
+            Move(_currentVelocity, ForceMode2D.Force);
+            SetDrag(0);
+            SetFriction(0);
+        }
+        else // if (isGrounded)
+        {
+            // Friction
+            SetDrag(9999);
+            SetFriction(9999);
+        }
     }
+
+    private void Move(Vector2 _velocity, ForceMode2D _forceMode) => Body2D.AddForce(_velocity, _forceMode);
+    private void SetDrag(float _drag) => Body2D.drag = _drag;
+    private void SetFriction(float _friction) => physicsMat.friction = _friction;
 }
---
+++ Assets/Scripts/Player/Input/InputClass.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class InputClass : MonoBehaviour
+{
+    InputMap Map;
+
+    #region Importants
+
+    private void Awake() => Map = new InputMap();
+    private void OnEnable() => Map.Enable();
+    private void OnDisable() => Map.Disable();
+
+    #endregion
+
+    #region Keys
+
+    public Vector2 MoveAxis() => Map.Player.MoveAxis.ReadValue<Vector2>();
+    public bool Jump() => Map.Player.Jump.ReadValue<float>() != 0;
+
+    #endregion
+}
